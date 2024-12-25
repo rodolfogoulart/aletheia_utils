4186df04fbf4feecbaa6c2b1a0088a9926d50c69
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2024-12-25 19:02:04 -0300

    export, bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
-## 0.0.1
+## 0.0.2
+* API for ip
 
-* TODO: Describe initial release.
+## 0.0.
+* initial release.
--- lib/aletheia_utils.dart
+++ lib/aletheia_utils.dart
@@ -1,7 +1,15 @@
 library aletheia_utils;
 
+//extensions
+export 'package:aletheia_utils/src/extensions.dart';
 //functions
 export 'package:aletheia_utils/src/function.dart';
-export 'package:aletheia_utils/src/extensions.dart';
 //singletown
 // export 'package:aletheia_utils/src/singletown/app.instance.dart';
+
+//api
+export 'package:aletheia_utils/src/api/ip/ip_api/ip.api.dart';
+export 'package:aletheia_utils/src/api/ip/ip_api/ip.api.model.dart';
+export 'package:aletheia_utils/src/api/ip/ip_registry/ipregistry.dart';
+export 'package:aletheia_utils/src/api/ip/ip_registry/ipregistry.model.dart';
+export 'package:aletheia_utils/src/api/ip/icanhazip.dart';
